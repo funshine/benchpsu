8cdc4d9386c77db4b80b7fe9cebc8a8a29b611f9
funshine <guijie.hong@gmail.com>
2016-05-01 00:44:28 +0800

    add inline style to component

--- app/mainWindow.jsx
+++ app/mainWindow.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import WeUI from 'react-weui';
 import 'weui';
+import { Sparklines, SparklinesLine, SparklinesSpots, SparklinesReferenceLine } from 'react-sparklines';
 import PowerValue from './powerValue';
+var update = require('react-addons-update');
 const {Button, Toast, ActionSheet, ButtonArea, Input} = WeUI;
 
 class App extends React.Component {
@@ -13,6 +15,13 @@ class App extends React.Component {
             showToast: false,
             showMenus: false,
             timer: null,
+            repeatTimer: null,
+            startButtonDisabled: false,
+            stopButtonDisabled: true,
+            pv: [],
+            pi: [],
+            nv: [],
+            ni: [],
             value: 0.0,
             menus: [{
                 label: '拍照',
@@ -38,6 +47,11 @@ class App extends React.Component {
         return (
             <section style={{ padding: `35px` }}>
                 <ButtonArea>
+                    <Button type="primary" disabled={this.state.startButtonDisabled} size="small" onClick={this.startRepeatTimer.bind(this) }>开始</Button>
+                    <Button type="warn" size="small" disabled={this.state.stopButtonDisabled} onClick={this.stopRepeatTimer.bind(this) }>停止</Button>
+                    <Button type="warn" size="small" onClick={this.clearPowerValue.bind(this) }>清除</Button>
+                </ButtonArea>
+                <ButtonArea>
                     <Button type="primary" size="small" onClick={this.showToast.bind(this) }>确认</Button>
                     <Button type="warn" size="small">注意</Button>
                     <Button type="primary" size="small" plain={true}>注意</Button>
@@ -45,8 +59,18 @@ class App extends React.Component {
                     <Button type="default" size="small" onClick={this.showMenus.bind(this) }>选择</Button>
                 </ButtonArea>
                 <PowerValue value={this.state.value}>6.66</PowerValue>
-                <PowerValue>-6.66</PowerValue>
-                <PowerValue>6.66</PowerValue>
+                <PowerValue value={this.state.pv[this.state.pv.length - 1]}>pv</PowerValue>
+
+                <Sparklines data={this.state.pv} limit={200} width={200} height={100} margin={10}>
+                    <SparklinesLine color="#56b45d" style={{ strokeWidth: 1, stroke: "#336aff", fill: "#56b45d" }} />
+                    <SparklinesSpots size={2}
+                        style={{ stroke: "#56b45d", strokeWidth: 1, fill: "#56b45d" }} />
+                    <SparklinesReferenceLine style={{ stroke: "#56b400" }} type="mean" />
+                </Sparklines>
+
+                <PowerValue type="warn" value={this.state.pi[this.state.pi.length - 1]}>pi</PowerValue>
+                <PowerValue value={this.state.nv[this.state.nv.length - 1]}>nv</PowerValue>
+                <PowerValue value={this.state.ni[this.state.ni.length - 1]}>ni</PowerValue>
                 <Input type="number" placeholder="请输入电压值" onChange={this.changeValue.bind(this) }/>
                 <Toast show={this.state.showToast}>loading...</Toast>
                 <ActionSheet show={this.state.showMenus} menus={this.state.menus} actions={this.state.actions} onRequestClose={this.hide.bind(this) } />
@@ -69,6 +93,31 @@ class App extends React.Component {
 
     changeValue(e) {
         this.setState({ value: Number(e.target.value) });
+    }
+    startRepeatTimer() {
+        if (this.state.startButtonDisabled) {
+            return;
+        }
+        this.state.repeatTimer = setInterval(() => {
+            var newValue = Math.random() * 0.01;
+            this.setState({
+                pv: this.state.pv.concat([5.095 + newValue]),
+                pi: this.state.pi.concat([1.495 + newValue]),
+                nv: this.state.nv.concat([-5.095 - newValue]),
+                ni: this.state.ni.concat([-0.495 - newValue])
+            });
+        }, 1000);
+        this.setState({ startButtonDisabled: true, stopButtonDisabled: false });
+    }
+    stopRepeatTimer() {
+        if (this.state.stopButtonDisabled) {
+            return;
+        }
+        clearInterval(this.state.repeatTimer);
+        this.setState({ startButtonDisabled: false, stopButtonDisabled: true });
+    }
+    clearPowerValue() {
+        this.setState({ pv: [0], pi: [0], nv: [0], ni: [0] });
     }
 }
 
--- app/powerValue.jsx
+++ app/powerValue.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
+var Radium = require('radium');
 
+@Radium
 class PowerValue extends React.Component {
     static propTypes = {
         value: React.PropTypes.number,
+        digit_num: React.PropTypes.number,
         disabled: React.PropTypes.bool,
         type: React.PropTypes.string,
         size: React.PropTypes.string
@@ -11,35 +14,50 @@ class PowerValue extends React.Component {
 
     static defaultProps = {
         value: 0.0,
+        digit_num: 4,
         disabled: false,
         type: 'primary',
         size: 'normal'
     };
 
     render() {
-        const {value, type, size, disabled, plain, className, children, ...others} = this.props;
-        const Component = this.props.href ? 'a' : 'button';
+        const {value, digit_num, type, size, disabled, plain, className, children, ...others} = this.props;
         const cls = classNames({
-            weui_btn: true,
-
-            weui_btn_primary: type === 'primary' && !plain,
-            weui_btn_default: type === 'default' && !plain,
-            weui_btn_warn: type === 'warn',
-
-            weui_btn_plain_primary: type === 'primary' && plain,
-
-            weui_btn_plain_default: type === 'default' && plain,
-
-            weui_btn_mini: size === 'small',
-
-            weui_btn_disabled: disabled,
-
             [className]: className
         });
 
         return (
-            <Component {...others} className={cls}>{value}</Component>
+            <div {...others} className={cls} style={[
+                styles.base,
+                styles[type]
+            ]} >{value.toFixed(digit_num) }</div>
         );
+    }
+};
+
+// You can create your style objects dynamically or share them for
+// every instance of the component.
+var styles = {
+    base: {
+        display: 'block',
+        textAlign: 'center',
+        color: '#fff',
+        background: '#56b45d',
+        fontSize: '9vh',
+        height: '10vh',
+        lineHeight: '10vh',
+        width: '40vw',
+        padding: '0px',
+        margin: '1vh',
+        borderRadius: '1vw'
+    },
+
+    primary: {
+        background: '#56b45d'
+    },
+
+    warn: {
+        background: '#FF4136'
     }
 };
 
